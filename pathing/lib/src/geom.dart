975db8ee0544753fd523e718d3176c7af73c0a4f
Ravi Teja Gudapati <tejainece@gmail.com>
2018-11-26 23:22:02 +0100

    Setting up for formation

---
+++ pathing/lib/src/geom.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+class Position {
+  int x;
+
+  int y;
+
+  Position({this.x: 0, this.y: 0});
+
+  Position get north => Position(x: x, y: y - 1);
+
+  Position get south => Position(x: x, y: y + 1);
+
+  Position get east => Position(x: x + 1, y: y);
+
+  Position get west => Position(x: x - 1, y: y);
+
+  Position get northEast => Position(x: x + 1, y: y - 1);
+
+  Position get southEast => Position(x: x + 1, y: y + 1);
+
+  Position get northWest => Position(x: x - 1, y: y - 1);
+
+  Position get southWest => Position(x: x - 1, y: y + 1);
+
+  List<Position> get neighbours =>
+      [north, northEast, east, southEast, south, southWest, west, northWest];
+
+  bool get hasNegative => x.isNegative || y.isNegative;
+
+  double distanceTo(Position other) {
+    final xDiff = x - other.x;
+    final yDiff = y - other.y;
+    return sqrt(xDiff * xDiff + yDiff * yDiff);
+  }
+}
---
+++ pathing/lib/src/unit.dart
@@ -0,0 +1,14 @@
+import 'package:pathing/src/geom.dart';
+
+class Unit {
+  int id;
+  Position pos;
+}
+
+class Game {
+  final buildings = <int, dynamic>{};
+  final units = <int, Unit>{};
+  final selects = <int, Unit>{};
+
+
+}
